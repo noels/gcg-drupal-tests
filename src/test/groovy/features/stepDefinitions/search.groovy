28ef55b3532137b94d59eda602b59c980f6ffd0e
Noel Sharpe <noels@radnetwork.co.uk>
2013-04-29 12:37:59 +0100

    add missing step to search steps

--- src/test/groovy/features/stepDefinitions/search.groovy
+++ src/test/groovy/features/stepDefinitions/search.groovy
@@ -8,6 +8,11 @@ Given(~'^I have entered search term into search box$') { ->
     //throw new PendingException()
 }
 
+
+When(~'^I press search$') { ->
+    // Express the Regexp above with the code you wish you had
+}
+
 Then(~'^I can see the search results$') { ->
     // Express the Regexp above with the code you wish you had
     //throw new PendingException()
